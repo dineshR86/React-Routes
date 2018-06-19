ec1556385862cb592ceb0bf396e1e7a6042b2fbd
dineshr86 <dinesh_gudapati@outlook.com>
2018-06-19 16:55:45 +0530

    Added Redux form

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,8 +1,21 @@
 import React,{Component} from 'react';
-
-export default class PostNew extends Component{
+import {Field,reduxForm} from 'redux-form';
+// reduxForm is a helper for connecting the form to the reducer (state). 
+// Field is used for interacting with the ReduxForm
+class PostNew extends Component{
 
     render(){
-        return <div>Post New Blog!!</div>
+        return (
+            <form>
+                <Field name="title" component={} />
+                <Field name="title" component={} />
+                <Field name="title" component={} />
+                </form>
+        );
     }
 }
+
+// the string being passed to the form element has to be unique. PostNewForm shld be unique value.
+export default reduxForm({
+    form:'PostNewForm'
+})(PostNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import {reducer as formReducer} from 'redux-form';
 import FetchPosts from './reducer_posts';
 
+
 const rootReducer = combineReducers({
-  posts:FetchPosts
+  posts:FetchPosts,
+  form: formReducer
 });
 
 export default rootReducer;
